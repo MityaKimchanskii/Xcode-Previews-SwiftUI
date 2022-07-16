0b6508c07502ac0bfdf02f38979970b7f9425709
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-16 13:53:40 -0700

    Initial Commit

---
+++ Xcode-Previews-SwiftUI/Xcode_Previews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Previews_SwiftUIApp.swift
+//  Xcode-Previews-SwiftUI
+//
+//  Created by Mitya Kim on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Previews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
